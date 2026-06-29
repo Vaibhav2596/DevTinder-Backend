43996f2a62ee666a27dc5dc88fc60e44be2a0b45
Vaibhav2596 <chavanv214@gmail.com>
2026-06-29 21:52:19 +0530

    Playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,72 +2,8 @@ const express = require("express");
 
 const app = express();
 
-// /user?userId=101&password=testing
-// this will only handle GET call to /users, /user, /user/xyz, /user/1
-// app.get("/user", (req, res) => {
-//   console.log(req.query);
-//   res.send({
-//     firstName: "Vaibhav",
-//     lastName: "Chavan",
-//   });
-// });
-
 app.get("/user/:userId/:name/:password", (req, res) => {
   console.log(req.params);
-  res.send({
-    firstName: "Vaibhav",
-    lastName: "Chavan",
-  });
-});
-
-
-// // ac, /abc
-// app.get("/ab?c", (req, res) => {
-//   res.send({
-//     firstName: "Vaibhav",
-//     lastName: "Chavan",
-//   });
-// });
-
-// // ac, /abc
-// app.get("/ab+c", (req, res) => {
-//   res.send({
-//     firstName: "Vaibhav",
-//     lastName: "Chavan",
-//   });
-// });
-
-// ac, /abc
-// app.get("/ab*cd", (req, res) => {
-//   res.send({
-//     firstName: "Vaibhav",
-//     lastName: "Chavan",
-//   });
-// });
-
-// app.get("/a(bc)?d", (req, res) => {
-//   res.send({
-//     firstName: "Vaibhav",
-//     lastName: "Chavan",
-//   });
-// });
-
-// app.get("/a(bc)+d", (req, res) => {
-//   res.send({
-//     firstName: "Vaibhav",
-//     lastName: "Chavan",
-//   });
-// });
-
-// Regex
-// app.get("/a/", (req, res) => {
-//   res.send({
-//     firstName: "Vaibhav",
-//     lastName: "Chavan",
-//   });
-// });
-
-app.get("/.*fly/", (req, res) => {
   res.send({
     firstName: "Vaibhav",
     lastName: "Chavan",
