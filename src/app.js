a701dcedb9052cabfbe088571c9d8da5b25d796b
Vaibhav2596 <chavanv214@gmail.com>
2026-07-01 21:59:40 +0530

    Middleware Usage

--- src/app.js
+++ src/app.js
@@ -1,28 +1,34 @@
 const express = require("express");
 
 const app = express();
- 
+
 // app.use("/route",rh1,rh2,rh3,rh4,rh5)
 // app.use("/route",[rh1,rh2,rh3,rh4,rh5])
 // app.use("/route",[rh1,rh2,rh3],rh4,rh5)
 
+// GET /users => It checks all the app.xxx("matching route") functions
+// GET /users => middleware chain => request handler
+
+app.use("/", (req,res,next) => {
+  // res.send("Handling / route");
+  next()
+});
+
 app.get(
   "/user",
   (req, res, next) => {
-    console.log("Handling the route user!!");
     next();
-  }
-);
-
-app.get(
-  "/user",
+  },
   (req, res, next) => {
-    console.log("Handling the route user 2!!");
-    res.send("2nd Route Handler")
-  }
+    next();
+  },
+  (req, res, next) => {
+    res.send("2nd Route Handler");
+  },
 );
-
 
 app.listen(7777, () => {
   console.log("Server is successfully listening on port 7777...");
 });
+
+// 46 minutes
