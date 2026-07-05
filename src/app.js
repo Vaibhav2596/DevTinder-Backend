1793664f593071a1244bc39c0abcf5db41da3c86
Vaibhav2596 <chavanv214@gmail.com>
2026-07-05 12:11:15 +0530

    WildCard Error Handling

--- src/app.js
+++ src/app.js
@@ -1,34 +1,24 @@
 const express = require("express");
-const {adminAuth, userAuth} = require("./middlewares/auth")
 
 const app = express();
 
-// app.use("/route",rh1,rh2,rh3,rh4,rh5)
-// app.use("/route",[rh1,rh2,rh3,rh4,rh5])
-// app.use("/route",[rh1,rh2,rh3],rh4,rh5)
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    // Log Your Error Message
+    res.status(500).send("Something Went Wrong!");
+  }
+});
 
-// GET /users => It checks all the app.xxx("matching route") functions
-// GET /users => middleware chain => request handler
+app.get("/getUserData", (req, res) => {
+  try {
+    // Logic of DB call and get user data
 
-// Handle Auth Middleware for all GET POST, ... requests
-app.use("/admin",adminAuth)
-
-app.post("/user/login",(req,res) => {
-  res.send("User logged in successfully!")
-})
-
-app.get("/user/data",userAuth, (req,res) => {
-  res.send("User Data Sent")
-})
-
-app.get("/admin/getAllData",(req, res, next) => {
-  res.send("All Data Sent")
-})
-
-app.get("/admin/deleteUser",(req, res, next) => {
-  // Logic of checking if the request is authorized
-  res.send("Deleted a user")
-})
+    throw new Error("sdfdsfds");
+    res.send("User Data Sent");
+  } catch (err) {
+    res.status(500).send("Some Error contact support team")
+  }
+});
 
 app.listen(7777, () => {
   console.log("Server is successfully listening on port 7777...");
