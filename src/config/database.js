4ceac4716bc73d1355182a6206a62eb81f7394e2
Vaibhav2596 <chavanv214@gmail.com>
2026-07-05 20:24:28 +0530

    Untrack src/config and add to gitignore

--- src/config/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = async() => {
-    await mongoose.connect("mongodb://vaibhavchavan40114_db_user:Vaibhav2508@ac-81v0mrm-shard-00-00.rnfqoln.mongodb.net:27017,ac-81v0mrm-shard-00-01.rnfqoln.mongodb.net:27017,ac-81v0mrm-shard-00-02.rnfqoln.mongodb.net:27017/devTinder?ssl=true&replicaSet=atlas-i9wfww-shard-0&authSource=admin&appName=VaibhavChavan");
-}
-
-module.exports = connectDB;
-
